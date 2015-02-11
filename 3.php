7e16bd43c90635bba474142f0604183a3ad37f12
SashaToropova <toropova.alya@mail.ru>
2015-02-11 21:19:12 +0600

    additionaly task 3

--- 3.php
+++ 3.php
@@ -4,6 +4,12 @@
 function do_something($param_1,$param_2)
 {
 printf("Line:%s\nFile:%s\nFunction:%s\n",__LINE__,__FILE__,__FUNCTION__);
+$numargs = func_num_args();
+$arg_list = func_get_args();
+for($i =1; $i<$numargs;$i++)
+{
+    echo "Parametr $i = ".$arg_list[$i]."\n";
+}
 return true;
 }
 do_something(4,5);
